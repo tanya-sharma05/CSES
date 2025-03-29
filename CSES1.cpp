be9d3e5ea8bea6b07c0cc943a07099dfadb8d1ac
Tanya <tanyasharma7658@gmail.com>
2025-03-29 23:14:54 +0530

    Add files via upload

---
+++ CSES1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long n;
+    cin>>n;
+    cout<<n<<" ";
+    while(n!=1){
+        if(n&1){
+            n=3*n+1;
+            cout<<n<<" ";
+        }
+        else{
+            n>>=1;
+            cout<<n<<" ";
+        }
+    }
+    return 0;
+}
