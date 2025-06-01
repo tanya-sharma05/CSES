070d8514b551f4d66b162c9597c19aace6d15ba5
Tanya <tanyasharma7658@gmail.com>
2025-06-01 20:53:28 +0530

    Update CSES01.cpp

--- CSES01.cpp
+++ CSES01.cpp
@@ -8,12 +8,11 @@ int main(){
     while(n!=1){
         if(n&1){
             n=3*n+1;
-            cout<<n<<" ";
         }
         else{
             n>>=1;
-            cout<<n<<" ";
         }
+        cout<<n<<" ";
     }
     return 0;
 }
