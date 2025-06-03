db98266e247042b6e690876821406669e26b895a
Tanya <tanyasharma7658@gmail.com>
2025-06-03 12:24:43 +0530

    Update and rename CSES3.cpp to CSES03.cpp

--- CSES03.cpp
+++ CSES03.cpp
@@ -4,9 +4,10 @@ using namespace std;
 int main(){
     string s;
     cin>>s;
+    int n=s.size();
     int len=1,maxLen=1;
-    for(int i=1;i<s.size();i++){
-        if(s[i]==s[i-1]){
+    for(int i=0;i<n-1;i++){
+        if(s[i]==s[i+1]){
             len++;
             maxLen=max(maxLen,len);
         }
