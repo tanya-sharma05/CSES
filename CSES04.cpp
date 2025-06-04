6913d4cfd452f6130924854ccfdf4d8a11263390
Tanya <tanyasharma7658@gmail.com>
2025-06-04 10:48:52 +0530

    Update and rename CSES4.cpp to CSES04.cpp

--- CSES04.cpp
+++ CSES04.cpp
@@ -1,17 +1,19 @@
+// Increasing array
 #include<bits/stdc++.h>
 using namespace std;
-
+ 
 int main(){
     int n;
     cin>>n;
-    long long a[n];
+    vector<int> a(n);
     for(int i=0;i<n;i++) cin>>a[i];
+    
     long long cnt=0,ans=0;
-    for(int i=1;i<n;i++){
-        if(a[i]<a[i-1]){
-            cnt=(a[i-1]-a[i]);
+    for(int i=0;i<n-1;i++){
+        if(a[i+1]<a[i]){
+            cnt=a[i]-a[i+1];
             ans+=cnt;
-            a[i]+=cnt;
+            a[i+1]+=cnt;
         }
     }
     cout<<ans<<endl;
